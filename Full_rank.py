0de864f18638389936f6c7f62eafa3872d9fdace
weiyinwei <weiyinwei@hotmail.com>
2022-04-21 10:43:36 +0800

    Update Full_rank.py

--- Full_rank.py
+++ Full_rank.py
@@ -16,18 +16,6 @@ def full_ranking(epoch, model, data, user_item_inter, mask_items, is_training, s
 
         print('---------------------------------{0}-th Precition:{1:.4f} Recall:{2:.4f} NDCG:{3:.4f}---------------------------------'.format(
             epoch, precision, recall, ndcg_score))
-        # if writer is not None:
-        #     writer.add_scalar(prefix+'_Precition', precision, epoch)
-        #     writer.add_scalar(prefix+'_Recall', recall, epoch)
-        #     writer.add_scalar(prefix+'_NDCG', ndcg_score, epoch)
-
-        #     writer.add_histogram(prefix+'_visual_distribution', model.v_rep, epoch)
-        #     writer.add_histogram(prefix+'_acoustic_distribution', model.a_rep, epoch)
-        #     writer.add_histogram(prefix+'_textual_distribution', model.t_rep, epoch)
-            
-        #     # writer.add_embedding(model.v_rep)
-        #     #writer.add_embedding(model.a_rep)
-        #     #writer.add_embedding(model.t_rep)
             
         return [precision, recall, ndcg_score]
 
